ee7961bf0043fbaf822d004577b5349bb9886e98
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-04-29 12:25:46 -0700

    WIP

---
+++ PDXBandIndex/Migrations/20220429185847_showTime.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PDXBandIndex.Migrations
+{
+    public partial class showTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Time",
+                table: "Shows",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "Shows");
+        }
+    }
+}
--- PDXBandIndex/Models/Show.cs
+++ PDXBandIndex/Models/Show.cs
@@ -15,6 +15,7 @@ namespace PDXBandIndex.Models
     public string Venue { get; set; }
     public DateTime Date { get; set; }
     public string Info { get; set; }
+    public string Time { get; set; }
     public virtual ICollection<BandShow> JoinEntities { get; set; }
   }
 }
